56cc34bd0ff881a6f9ca2a4c024b0a0bb8f95e6f
luizhenriqque18 <luizhenriqque15@gmail.com>
2021-01-30 22:43:45 -0400

    feat components input and button

---
+++ src/components/Button/index.js
@@ -0,0 +1,34 @@
+import React, {} from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const CustomButton = (props) => {
+
+    return (
+        <View style={style.view}>
+            <TouchableOpacity {...props} style={style.button}>
+                <Text style={style.text}>{props.title}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    button: {
+        backgroundColor: '#001B2E',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 10,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: "500"
+    },
+    view: {
+        width: '100%'
+    }
+})
+
+export default CustomButton;
---
+++ src/components/TextInput/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+const CustomTextInput = (props) => {
+    return(
+        <View style={style.view}>
+            <Text style={style.text}>{props.title}</Text>
+            <TextInput style={style.input} placeholder={'000000000000'} />
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    view: {
+        backgroundColor: '#fff',
+        borderBottomColor: '#001B2E',
+        borderBottomWidth: 1,
+        width: '100%'
+    },
+    text: {
+        width: '100%',
+        paddingBottom: 0,
+        fontSize: 14,
+        fontWeight: '400',
+    },
+    input: {
+        width: '100%',
+        paddingBottom: 5,
+        paddingTop: 0,
+        paddingLeft: 0,
+        fontSize: 14,
+        fontWeight: '400',
+    }
+});
+
+export default CustomTextInput;
